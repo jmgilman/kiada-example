0bf19160a8e39ffa29570ea4a6a3cdf2716d2f9d
Joshua Gilman <joshuagilman@gmail.com>
2022-12-28 16:10:56 -0800

    build: adds script for local cluster init with kind

---
+++ scripts/local_init.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Create the cluster
+echo ">>> Creating cluster..."
+kind create cluster --config "${PRJ_ROOT}/scripts/manifests/kind.yml"
+
+# Install Calico
+echo ">>> Installing Calico..."
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+# Wait for calico
+echo ">>> Waiting 60 seconds for Calico..."
+sleep 60
+
+# Install MetalLB
+echo ">>> Installing MetalLB..."
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
+
+# Wait for MetalLB
+echo ">>> Waiting for MetalLB to come up..."
+kubectl wait --namespace metallb-system \
+    --for=condition=ready pod \
+    --selector=app=metallb \
+    --timeout=180s
+
+# Configure MetalLB
+echo ">>> Configuring MetalLB..."
+kubectl apply -f "${PRJ_ROOT}/scripts/manifests/metallb.yml"
+
+echo ">>> Done!"
