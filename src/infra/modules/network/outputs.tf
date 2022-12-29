f0a007b60a46d67f9e9501c12a2db4c1008edbf4
Joshua Gilman <joshuagilman@gmail.com>
2022-12-29 12:42:40 -0800

    refactor: pipes all outputs from base vpc module

--- src/infra/modules/network/outputs.tf
+++ src/infra/modules/network/outputs.tf
@@ -1,14 +1,4 @@
-output "vpc_id" {
-  description = "vpc_id"
-  value       = module.this.vpc_id
-}
-
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.this.private_subnets
-}
-
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.this.public_subnets
+output "vpc" {
+  description = "Outputs from the VPC module"
+  value       = module.this
 }
