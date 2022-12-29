10a9d8dd39d5e7082925a615fad18681e331d069
Joshua Gilman <joshuagilman@gmail.com>
2022-12-28 16:31:25 -0800

    feat: adds initial network configuration

---
+++ src/infra/dev/network/main.tf
@@ -0,0 +1,20 @@
+data "aws_region" "current" {}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = [
+    "${aws_region.current.name}a",
+    "${aws_region.current.name}b",
+    "${aws_region.current.name}c",
+  ]
+
+  private_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets   = ["10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  database_subnets = ["10.0.21.0/24", "10.0.22.0/24", "10.0.23.0/24"]
+
+  enable_nat_gateway = true
+}
